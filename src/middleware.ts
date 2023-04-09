9a46a3acc399fb7274e64e767993a9e60dd54a1d
addania <mirka.schw@gmail.com>
2023-04-09 12:45:25 +0200

    Add sign in/out

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export default withClerkMiddleware(() => {
+  console.log("I am running");
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/((?!static|.*\\..*|_next|favicon.ico).*)", "/"],
+};
