1c581ed31dcb614428b64ee02d895044833acf52
addania <mirka.schw@gmail.com>
2023-04-13 20:33:11 +0200

    Add images and more post info , styling up

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,40 @@
+import clerkClient, { User } from "@clerk/clerk-sdk-node";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profilePicture: user.profileImageUrl,
+  };
+};
+
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+      where: { authorId: "user_2OBaeJj8EI29omUN4LZTUFl7TBh" },
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+
+      if (!author || !author.username) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author of the posts not found",
+        });
+      }
+      return { post, author: { ...author, username: author.username } };
+    });
   }),
 });
